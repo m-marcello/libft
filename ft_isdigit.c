d9bf8ddb2ebe0b2c2872d09b432cb3203fee5648
Moana-rose Marcello <mmarcell@f1r2s7.codam.nl>
2020-01-18 17:06:45 +0100

    optimise ft_isdigit

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,13 +6,11 @@
 /*   By: mmarcell <mmarcell@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/16 20:04:43 by mmarcell       #+#    #+#                */
-/*   Updated: 2019/01/25 18:32:10 by mmarcell      ########   odam.nl         */
+/*   Updated: 2020/01/18 16:51:21 by mmarcell      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_isdigit(int c)
 {
-	if ('0' <= c && c <= '9')
-		return (1);
-	return (0);
+	return ('0' <= c && c <= '9');
 }
