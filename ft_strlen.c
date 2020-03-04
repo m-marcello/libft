b38e5bae38292b6e11bbc24bdc0baff7897f1ac5
Moana-rose Marcello <mmarcell@f1r2s16.codam.nl>
2020-03-04 17:58:52 +0100

    roll back the change in ft_strlen() because a null pointer is not an empty string and therefore the return shouldn't be the same

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: mmarcell <mmarcell@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/16 19:55:53 by mmarcell       #+#    #+#                */
-/*   Updated: 2020/03/04 17:37:58 by mmarcell      ########   odam.nl         */
+/*   Updated: 2020/03/04 17:55:06 by mmarcell      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ size_t	ft_strlen(const char *s)
 {
 	size_t	len;
 
-	if (s == 0)
-		return (0);
 	len = 0;
 	while (s[len] != 0)
 		len++;
