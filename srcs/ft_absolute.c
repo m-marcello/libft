37d6f538ad7cc64eac9088436a65595e90bb6af5
mocello <mmarcell@student.codam.nl>
2020-04-23 16:59:07 +0200

    fix function name in ft_absolute()

--- srcs/ft_absolute.c
+++ srcs/ft_absolute.c
@@ -6,11 +6,11 @@
 /*   By: moana <moana@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/04/17 18:35:38 by moana         #+#    #+#                 */
-/*   Updated: 2020/04/17 18:36:43 by moana         ########   odam.nl         */
+/*   Updated: 2020/04/23 16:54:02 by moana         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-long long int	abolute(long long int nbr)
+long long int	ft_absolute(long long int nbr)
 {
 	if (nbr < 0)
 		return (-1 * nbr);
