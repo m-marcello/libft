9c130e20455041461964f0284ab6b81a1819af97
moana <moanamarcello@gmail.com>
2020-07-03 17:12:03 +0200

    rewrite ft_lstrec() to avoid segfaults

--- srcs/ft_lstrev.c
+++ srcs/ft_lstrev.c
@@ -12,15 +12,35 @@
 
 #include "libft.h"
 
+// t_list	*ft_lstrev(t_list **alst)
+// {
+// 	t_list *curr;
+
+// 	if (!alst || !(*alst))
+// 		return (NULL);
+// 	curr = ft_lstdequeue(alst);
+// 	curr->next = NULL;
+// 	while (alst)
+// 		ft_lstadd(&curr, ft_lstdequeue(alst));
+// 	return (curr);
+// }
+
 t_list	*ft_lstrev(t_list **alst)
 {
-	t_list *curr;
+	t_list	*curr;
+	t_list	*old_list;
+	t_list	*new_head;
 
-	if (!alst || !(*alst))
+	if (alst == NULL || *alst == NULL)
 		return (NULL);
-	curr = ft_lstdequeue(alst);
-	curr->next = NULL;
-	while (alst)
-		ft_lstadd(&curr, ft_lstdequeue(alst));
-	return (curr);
+	old_list = (*alst);
+	new_head = NULL;
+	while (old_list != NULL)
+	{
+		curr = old_list;
+		old_list = old_list->next;
+		curr->next = NULL;
+		ft_lstadd(&new_head, curr);
+	}
+	return (new_head);
 }
